a34e17fd7b1cef6eb7c0a64d68af90e605607921
ecucuzzella <78390916+ecucuzzella@users.noreply.github.com>
2025-05-20 11:28:09 -0400

    Add files via upload

---
+++ Presentation-Examples/ui.R
@@ -0,0 +1,30 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Telephones by region"),
+  
+  p("This is a plot showing different telephones by regions from data provided by AT&T"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones)),
+      hr(),
+      helpText("Data from AT&T (1961) The World's Telephones.")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("phonePlot")  
+    )
+    
+  )
+)
